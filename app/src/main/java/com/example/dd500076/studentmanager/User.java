b9a701f2a6598c0691dbb9e312ef2502bdb1d235
DreamTeamIUT <dylan.delaporte@etu.unice.fr>
2017-02-27 11:00:50 +0100

    list students

---
+++ app/src/main/java/com/example/dd500076/studentmanager/User.java
@@ -0,0 +1,21 @@
+package com.example.dd500076.studentmanager;
+
+/**
+ * Created by dd500076 on 27/02/17.
+ */
+
+public class User {
+    public String name;
+    public String idEtu;
+    public String surname;
+    public String studies;
+    public int year;
+
+    public User(String idEtu, String name, String surname, String studies, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.studies = studies;
+        this.idEtu = idEtu;
+        this.year = year;
+    }
+}
